042f98ae370aab0c7be3d1029bb29ea4896ce825
dlwr <yuta25.a.k.a.djbudo.a.k.a.dlwr@gmail.com>
2026-08-21 17:27:11 +0900

    Remixサンプルを削除しVite+Cloudflare構成に刷新

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-import { vitePlugin as remix } from "@remix-run/dev";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    remix({
-      ssr: false,
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [cloudflare()],
 });
